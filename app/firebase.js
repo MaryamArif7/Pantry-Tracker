a03e930e50e191cc6eb8af7555e315574117e38d
MaryamArif7 <www.maryamarif28242@gmail.com>
2024-08-04 20:35:56 +0500

    removed firebase analytics

--- app/firebase.js
+++ app/firebase.js
@@ -23,7 +23,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 const firestore = getFirestore(app);
 const auth = getAuth(app); // Initialize Auth
 
