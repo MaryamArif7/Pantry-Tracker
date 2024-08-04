f75b1d8afa0f3ce436ef3d16b545d88d7bf1183d
MaryamArif7 <www.maryamarif28242@gmail.com>
2024-08-04 09:52:23 +0500

    setting up auth

--- README.md
+++ README.md
@@ -34,3 +34,14 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+/*    --primary-100:#6c35de;
+    --primary-200:#a364ff;
+    --primary-300:#ffc7ff;
+    --accent-100:#cb80ff;
+    --accent-200:#373737;
+    --text-100:#ffffff;
+    --text-200:#e0e0e0;
+    --bg-100:#241b35;
+    --bg-200:#342a45;
+    --bg-300:#4d425f;
+     */
--- app/page.js
+++ app/page.js
@@ -1,167 +1,40 @@
-"use client";
-import { useState, useEffect } from "react";
-import {
-  collection,
-  deleteDoc,
-  getDoc,
-  getDocs,
-  query,
-  setDoc,
-  doc,
-} from "firebase/firestore";
-import { firestore } from "./firebase";
-import {
-  Modal,
-  Box,
-  Typography,
-  Stack,
-  TextField,
-  Button,
-} from "@mui/material";
+import "./globals.css";
+import Link from "next/link";
 
 export default function Home() {
-  const [inventory, setInventory] = useState([]);
-  const [itemName, setItemName] = useState("");
-  const [open, setOpen] = useState(false);
-
-  const updateInventory = async () => {
-    const snapshot = query(collection(firestore, "inventory"));
-    const docs = await getDocs(snapshot);
-    const inventoryList = [];
-    docs.forEach((doc) => {
-      inventoryList.push({
-        name: doc.id,
-        ...doc.data(),
-      });
-    });
-    setInventory(inventoryList);
-  };
-
-  const addItem = async (item) => {
-    const docRef = doc(firestore, "inventory", item);
-    const docSnap = await getDoc(docRef);
-    if (docSnap.exists) {
-      await deleteDoc(docRef);
-    } else {
-      await setDoc(docRef, { count: 1 });
-    }
-    await updateInventory();
-  };
-
-  const removeItem = async (item) => {
-    const docRef = doc(firestore, "inventory", item);
-    const docSnap = await getDoc(docRef);
-    if (docSnap.exists) {
-      const { count } = docSnap.data();
-      await setDoc(docRef, { count: count + 1 });
-    } else {
-      await setDoc(docRef, { count: count - 1 });
-    }
-    await updateInventory();
-  };
-
-  useEffect(() => {
-    updateInventory();
-  }, []);
-
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
   return (
-    <Box
-      width="100vw"
-      height="100vh"
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      flexDirection="column"
-      gap={2}
-    >
-      <Modal open={open} onClose={handleClose}>
-        <Box
-          position="absolute"
-          top="50%"
-          left="50%"
-          transform="translate(-50%,-50%)"
-          width={400}
-          bgcolor="white"
-          border="2px solid #000"
-          boxShadow={24}
-          p="4"
-          display="flex"
-          flexDirection="column"
-          gap={3}
-          sx={{
-            transform: "translate(-50%,-50%)",
-          }}
-        >
-          <Typography variant="h6">Add Item</Typography>
-
-          <Stack width="100%" direction="row" spacing={2}>
-            <TextField
-              variant="outlined"
-              fullWidth
-              value={itemName}
-              onChange={(e) => setItemName(e.target.value)}
-            />
-            <Button
-              variant="outlined"
-              onClick={() => {
-                addItem(itemName);
-                setItemName("");
-                handleClose();
-              }}
-            >
-              Add{" "}
-            </Button>
-          </Stack>
-        </Box>
-      </Modal>
-
-      <Button variant="contained" onClick={handleOpen}>
-        Add new Item
-      </Button>
-      <Box border="1px solid #333">
-        <Box
-          width="800px"
-          height="100px"
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          bgcolor="#ADD8E6"
-        >
-          <Typography variant="h2" color="#333">
-            inventory Items
-          </Typography>
-        </Box>
-      </Box>
-      <Stack width="800px" height="300px" spacing={2} overflow="auto">
-        {inventory.map(({ name, count }) => (
-          <Box
-            key={name}
-            width="100%"
-            minHeight="150px"
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            bgcolor="#f0f0f0"
-            padding={5}
-          >
-            <Typography variant="h3" color="#333" textAlign="center">
-              {name}
-            </Typography>
-            <Typography variant="h3" color="#333" textAlign="center">
-              {count}
-            </Typography>
-            <Button variant="contained"  
-            onClick={()=>{
-              removeItem(name)
-            }}
-            
-            >Remove Items</Button>
-          </Box>
-        ))}
-      </Stack>
-    </Box>
+    <>
+      <div className="flex items-center ">
+        <div className="text-3xl text-green-400">🥫</div>
+        <span className="text-white text-2xl font-bold">PantryCheck</span>
+      </div>
+      <div className="flex">
+        <div className="mt-16">
+          <h1 className="text-8xl">Welcome</h1>
+          <h1 className="text-4xl">
+            Smart Inventory Management system at <br /> your fingertips
+          </h1>
+         <div className="mt-10 ">
+         <Link href="/signup" className=" bg-purple-500 text-white py-3 px-6 rounded-lg hover:bg-purple-900">
+            Get Started
+          </Link>
+         </div>
+        </div>
+      </div>
+      <div className="flex gap-3 mt-10">
+        <div className="bg-customPurple hover:bg-onHover flex justify-center items-center w-52 h-32 bg-opacity-50 backdrop-filter backdrop-blur-lg rounded-lg shadow-lg">
+          Cook with confidence
+        </div>
+        <div className="hover:bg-onHover bg-customPurple flex justify-center items-center w-52 h-32 bg-opacity-50 backdrop-filter backdrop-blur-lg rounded-lg shadow-lg">
+          Easy to use
+        </div>
+        <div className="bg-customPurple hover:bg-onHover flex justify-center items-center w-52 h-32 bg-opacity-50 backdrop-filter backdrop-blur-lg rounded-lg shadow-lg">
+          User Friendly
+        </div>
+        <div className="bg-customPurple hover:bg-onHover flex justify-center items-center w-52 h-32 bg-opacity-50 backdrop-filter backdrop-blur-lg rounded-lg shadow-lg">
+          Manage Your Pantries
+        </div>
+      </div>
+    </>
   );
 }
